5203c98d28888e62f2e769858c39ad5ba353c595
eusouotopo-stack <eusouotopo@gmail.com>
2025-11-07 23:44:26 +0200

    Create generator.js

---
+++ src/components/src/components/src/components/pages/generator.js
@@ -0,0 +1,4 @@
+import { useState } from "react";
+import MemeCanvas from "../src/components/MemeCanvas.js";
+import AdBanner from "../src/components/AdBanner.js";
+import CookieBanner from "../src/components/CookieBanner.js";
